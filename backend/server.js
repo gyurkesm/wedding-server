6b1005dcf97390298e3b7a7b927367fab29042a4
Martin <gyurkes.martin@gmail.com>
2023-12-13 20:36:00 +0100

    CRUD API DONE

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 5000;
+const colors = require('colors');
+const connectDB = require('./config/db')
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler);
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`));
