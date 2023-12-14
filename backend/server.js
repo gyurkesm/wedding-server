f404fff72d98be267d543bfcaadc63ebd0c1fa9f
Martin <gyurkes.martin@gmail.com>
2023-12-14 21:23:13 +0100

    guest route

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/guest', require('./routes/guestRoutes'));
 
 app.use(errorHandler);
 
