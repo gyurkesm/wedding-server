f7521a0c48e00c69c1e52a06191d0ca02b1b9023
Martin <gyurkes.martin@gmail.com>
2023-12-29 18:35:54 +0100

    vue teszt

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
-const colors = require('colors');
-const connectDB = require('./config/db')
+const colors = require("colors");
+const connectDB = require("./config/db");
 const app = express();
 // const cors = require('cors');
 
-connectDB(app.listen(port, ()=> console.log(`Server started on port ${port}`)));
-
+connectDB(
+  app.listen(port, () => console.log(`Server started on port ${port}`))
+);
 
 app.use(express.urlencoded({ extended: false }));
 
-
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://lottimartin.com');
-  
+  //res.setHeader('Access-Control-Allow-Origin', 'https://lottimartin.com');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   res.header(
-    'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  
+
   next();
-})
+});
 
 // app.use(cors());
 app.use(express.json({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/guest', require('./routes/guestRoutes'));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/guest", require("./routes/guestRoutes"));
 
 app.use(errorHandler);
 
-app.get('/get-ip', (req, res) => {
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.get("/get-ip", (req, res) => {
+  const clientIp =
+    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   res.send(`Client IP Address: ${clientIp}`);
 });
